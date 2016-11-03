67ee521e3110c4e5036e57fb0736d0aa350ce5e9
Zac Clemans <zaclem01@gmail.com>
2016-11-02 22:07:22 -0400

    correct and add rxjs imports to markdown service

--- src/app/markdown/markdown.service.ts
+++ src/app/markdown/markdown.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
-import 'rxjs/RX';
+import 'rxjs/Rx';
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class MarkdownService {
