e411c00818cda6839cc01a8d809ee353d36d269f
Daniel Sokolowski <daniel@sokolowski.org>
2017-08-08 22:23:26 +0200

    Fixed marked.js import for es modules

--- src/markdown/markdown.service.ts
+++ src/markdown/markdown.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Response} from '@angular/http';
 import { Observable } from 'rxjs/Observable';
-import * as  marked from 'marked';
+import marked from 'marked';
 
 import 'rxjs/add/observable/throw';
 import 'rxjs/add/operator/catch';
