3cbf16f1cbbf746a5a3279195fb3e31b7ba0a499
Dimpu <buddhaaravind@gmail.com>
2019-03-03 15:09:51 -0500

    feature/code-coverage

--- projects/ngx-md/src/lib/ngx-md.service.spec.ts
+++ projects/ngx-md/src/lib/ngx-md.service.spec.ts
@@ -1,57 +1,60 @@
 import { TestBed, async, inject } from '@angular/core/testing';
-import {
-  HttpClientModule,
-  HttpClient,
-  HttpResponse,
-  HttpXhrBackend,
-  HttpBackend
-} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
-import {
-  HttpClientTestingModule,
-  HttpTestingController
-} from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { NgxMdService } from './ngx-md.service';
-import { Observable, of } from 'rxjs';
+
 
 describe('NgxMdService', () => {
-  let httpMock: HttpTestingController;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
   let markdownService: NgxMdService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
-      providers: [NgxMdService]
+      imports: [ HttpClientTestingModule ],
+      providers: [ NgxMdService ]
     });
 
     // Inject the http service and test controller for each test
-    httpMock = TestBed.get(HttpTestingController);
+    httpClient = TestBed.get(HttpClient);
+    httpTestingController = TestBed.get(HttpTestingController);
+    markdownService = TestBed.get(NgxMdService);
   });
+
   afterEach(() => {
-    httpMock.verify();
+    httpTestingController.verify();
   });
 
   it(
     'should be created',
-    inject([NgxMdService], (service: NgxMdService) => {
+    inject([ NgxMdService ], (service: NgxMdService) => {
       expect(service).toBeTruthy();
     })
   );
 
-  beforeEach(() => {
-    markdownService = TestBed.get(NgxMdService);
-    const mockResponse = `Data`;
+  it('should fail with 404 to get [path] content', () => {
+    const emsg = 'deliberate 404 error';
+    const testUrl = 'some/path';
+    markdownService.getContent('some/path').subscribe(
+      (data) => fail('should have failed with the 404 error'),
+      (error: any) => {
+        expect(error).toEqual('Http failure response for some/path: 404 Not Found', 'status');
+      }
+    );
+    const req = httpTestingController.expectOne(testUrl);
+    req.flush(emsg, { status: 404, statusText: 'Not Found' });
   });
 
   it('should call http service to get [path] content', () => {
     const TEST_DATA = 'Foo';
     const TEST_URL = 'src-x';
 
-    markdownService.getContent(TEST_URL).subscribe(received => {
+    markdownService.getContent(TEST_URL).subscribe((received) => {
       expect(received).toEqual(TEST_DATA);
     });
 
-    const req = httpMock.expectOne(TEST_URL);
+    const req = httpTestingController.expectOne(TEST_URL);
     expect(req.request.method).toBe('GET');
     req.flush(TEST_DATA);
   });
@@ -64,42 +67,59 @@ describe('NgxMdService', () => {
       const TEST_DATA = 'Foo';
       const TEST_URL = 'src-x';
 
-      markdownService.getContent(TEST_URL).subscribe(responseData => {
+      markdownService.getContent(TEST_URL).subscribe((responseData) => {
         expect(markdownService.extractData).toHaveBeenCalledWith(TEST_DATA);
       });
 
-      const req = httpMock.expectOne(TEST_URL);
+      const req = httpTestingController.expectOne(TEST_URL);
       expect(req.request.method).toBe('GET');
       req.flush(TEST_DATA);
     })
   );
 
-  xdescribe('XSS protection', () => {
+  describe('XSS protection', () => {
     describe('Sanitize enabled', () => {
       it('should not change safe links', () => {
-        expect(markdownService.compile('[test](foo)').trim())
-          .toEqual('<p><a href="foo">test</a></p>');
-        expect(markdownService.compile('[test](http://example.com)').trim())
-          .toEqual('<p><a href="http://example.com">test</a></p>');
-        expect(markdownService.compile('[test](mailto:foo@example.com)').trim())
-          .toEqual('<p><a href="mailto:foo@example.com">test</a></p>');
+        expect(markdownService.compile('[test](foo)').trim()).toEqual('<p><a href="foo">test</a></p>');
+        expect(markdownService.compile('[test](http://example.com)').trim()).toEqual(
+          '<p><a href="http://example.com">test</a></p>'
+        );
+        expect(markdownService.compile('[test](mailto:foo@example.com)').trim()).toEqual(
+          '<p><a href="mailto:foo@example.com">test</a></p>'
+        );
       });
       it('should change unsafe links', () => {
-        expect(markdownService.compile('[Click Me](javascript:alert(&#39;Injected!&#39;%29)').trim())
-          .toEqual('<p><a href="unsafe:javascript:alert(\'Injected!\'%29">Click Me</a></p>');
+        expect(markdownService.compile('[Click Me](javascript:alert(\'Injected!\'))').trim()).toEqual(
+          '<p><a href="unsafe:javascript:alert(\'Injected!\')">Click Me</a></p>'
+        );
       });
     });
+
     describe('Sanitize disabled', () => {
       it('should not change safe links', () => {
-        expect(markdownService.compile('[test](foo)', false).trim())
-          .toEqual('<p><a href="foo">test</a></p>');
-        expect(markdownService.compile('[test](http://example.com)', false).trim())
-          .toEqual('<p><a href="http://example.com">test</a></p>');
-        expect(markdownService.compile('[test](mailto:foo@example.com)', false).trim())
-          .toEqual('<p><a href="mailto:foo@example.com">test</a></p>');
-        expect(markdownService.compile('[Click Me](javascript:alert(&#39;Injected!&#39;%29)', false).trim())
-          .toEqual('<p><a href="javascript:alert(&#39;Injected!&#39;%29">Click Me</a></p>');
+        expect(markdownService.compile('[test](foo)', false).trim()).toEqual('<p><a href="foo">test</a></p>');
+        expect(markdownService.compile('[test](http://example.com)', false).trim()).toEqual(
+          '<p><a href="http://example.com">test</a></p>'
+        );
+        expect(markdownService.compile('[test](mailto:foo@example.com)', false).trim()).toEqual(
+          '<p><a href="mailto:foo@example.com">test</a></p>'
+        );
+        expect(
+          markdownService.compile('[Click Me](javascript:alert(\'Injected!\'))', false).trim()
+        ).toEqual('<p><a href="javascript:alert(&#39;Injected!&#39;)">Click Me</a></p>');
       });
     });
   });
+
+
+  describe('Marked Js', () => {
+    it('should get render', () => {
+      expect(markdownService.renderer).toBeTruthy();
+    });
+
+    it('should extendRenderer return checkbox', () => {
+
+    });
+  });
+
 });
