9df570f525261c1c31053e39d7d884db86587eb8
dbuddha <dbuddha@snapchat.com>
2022-06-05 20:49:46 -0700

    old

--- apps/ngx-md-demo-e2e/src/integration/app.spec.ts
+++
@@ -1,9 +0,0 @@
-import { getGreeting } from '../support/app.po';
-
-describe('ngx-md-demo', () => {
-  beforeEach(() => cy.visit('/'));
-
-  it('should display welcome message', () => {
-    getGreeting().contains('Welcome to ngx-md-demo!');
-  });
-});
--- libs/ngx-md/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'ngx-md',
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/libs/ngx-md',
-  snapshotSerializers: [
-    'jest-preset-angular/AngularSnapshotSerializer.js',
-    'jest-preset-angular/HTMLCommentSerializer.js'
-  ]
-};
